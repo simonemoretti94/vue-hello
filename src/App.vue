668e0519bf016cea5587d9f5f37f53cb3d586628
simonemoretti94 <simone.moretti.944@gmail.com>
2024-01-21 23:05:05 +0100

    2th added some features

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,25 @@ export default {
   data() {
     return {
       greetings: 'Welcome to vue-hello repo!',
-      name: '*',
-      surname: '*',
+      name: '',
+      surname: '',
     }
   },
   methods: {
-
+    lengthCalculator(nome, cognome) {
+      return nome.length + cognome.length;
+    },
+    evenOrOdd(nameSurnameLength) {
+      if (nameSurnameLength % 2 === 0 && nameSurnameLength !== 0) {
+        return 'is even!';
+      }
+      else if (nameSurnameLength === 0) {
+        return 'is null!';
+      }
+      else {
+        return 'is odd!';
+      }
+    }
   },
 }
 
@@ -34,6 +47,15 @@ export default {
       <label for="surname">Surname</label>
       <input type="text" v-model="surname">
     </div>
+
+    <div id="div-type2">
+      <p>So you are {{ name + ' ' + surname }}, even {{ lengthCalculator(name, surname) }} characters!</p>
+    </div>
+
+    <div id="div-type3">
+      <p>{{ lengthCalculator(name, surname) }} {{ evenOrOdd(lengthCalculator(name, surname)) }}</p>
+    </div>
+
 
   </main>
 </template>
@@ -77,7 +99,7 @@ main {
 
     & label {
       text-align: center;
-      margin-left: .25rem;
+      margin-left: .5rem;
 
     }
 
@@ -86,6 +108,27 @@ main {
       border-radius: 10px;
     }
 
+  }
+
+  & #div-type2 {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    border: dashed 2px white;
+    border-radius: 15px;
+  }
+
+  #div-type3 {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    border: groove 2px rgb(95, 191, 254);
+    border-radius: 20px;
+    box-shadow: 4px 6px rgb(179, 179, 179);
   }
 }
 </style>
